b97057820574fd230578be80dccd2105417abd82
Vitor Aguiar <vitor.aguiar.8960@gmail.com>
2026-05-23 15:31:11 -0300

    feat: create energy wise landing page

---
+++ script.js
@@ -0,0 +1,26 @@
+// SCROLL SUAVE
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+  anchor.addEventListener('click', function(e){
+
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href'))
+      .scrollIntoView({
+        behavior: 'smooth'
+      });
+
+  });
+
+});
+
+
+// QR CODE AUTOMÁTICO
+
+const qrCode = document.getElementById("qrCode");
+
+const projectUrl = "https://energywise1.netlify.app/";
+
+qrCode.src =
+  `https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=${encodeURIComponent(projectUrl)}`;
